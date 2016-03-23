82b0d92338bcde678f29f9cec9c874328343bbde
crollinson <crollinson@gmail.com>
2016-03-23 11:05:43 -0400

    runs post process file path fix

--- MIP2_Region/4_runs/start_runs_spawn.sh
+++ MIP2_Region/4_runs/start_runs_spawn.sh
@@ -177,7 +177,7 @@ do
 		cp ${setup_dir}cleanup_runs.sh .
 		paleon_out=${file_path}/${SITE}_paleon		
 	    sed -i "s,TEST,post_${SITE},g" sub_post_process_runs.sh # change job name
-	    sed -i "s,/dummy/path,${paleon_out},g" sub_post_process_runs.sh # set the file path
+	    sed -i "s,/dummy/path,${file_path},g" sub_post_process_runs.sh # set the file path
 
 		sed -i "s/SITE=.*/SITE=${SITE}/" post_process_runs.sh 		
 		sed -i "s/job_name=.*/job_name=extract_${SITE}/" post_process_runs.sh 		

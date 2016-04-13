6d8a317faef5ae0a0d49a16b5ffa47a1e2960441
crollinson <crollinson@gmail.com>
2016-04-13 11:14:39 -0400

    Status update with map
    also fixed a bug with the colors that caused things to go wonky when
    there’s only 2 statuses

--- MIP2_Region/0_setup/map_status.R
+++ MIP2_Region/0_setup/map_status.R
@@ -6,6 +6,13 @@ paleon.states <- map_data("state")
 paleon.status$Status <- as.factor(ifelse(is.na(paleon.status$runs), "in queue", ifelse(substr(paleon.status$runs,1,1)=="*", "in progress", "completed")))
 summary(paleon.status)
 
+
+if(length(unique(paleon.status$Status))==3){ 
+  stat.color <- c("red", "blue", "gray50") 
+} else{ 
+  stat.color<-c("red", "gray50")
+}
+
 png("ED_Regional_Status.png", height=4, width=10, units="in", res=180)
 ggplot() +
 	geom_raster(data=paleon.status, aes(x=lon, y=lat, fill=Status)) +
@@ -13,7 +20,7 @@ ggplot() +
 	# geom_point(aes(x=lon, y=lat), size=1, color="blue") +
     scale_x_continuous(limits=range(paleon.status$lon), expand=c(0,0), name="Longitude (degrees)") +
     scale_y_continuous(limits=range(paleon.status$lat), expand=c(0,0), name="Latitude (degrees)") +
-    scale_fill_manual(values=c("red", "blue", "gray50")) +
+    scale_fill_manual(values=stat.color) +
 	coord_equal(ratio=1) +
 	theme_bw()
 dev.off()

4a83badf515ed14f92c89c0af1a9252d722c9ee1
Christy Rollinson <crollinson@gmail.com>
2017-04-24 13:34:41 -0500

    Don't restrict ED runs to geo (which is being slow)
    We’re hitting crunch time, so lets burn through the Dietze Lab cluster
    budget to get it done.  (This was approved by Mike)

--- MIP2_Region/0_setup/paleon_ed2_smp_geo.sh
+++ MIP2_Region/0_setup/paleon_ed2_smp_geo.sh
@@ -5,8 +5,8 @@
 #$ -V 
 #$ -pe omp 4
 #$ -v OMP_NUM_THREADS=4
-#$ -q "geo*"
-#$ -l hostname=!scc-c*&!scc-t*
+# #$ -q "geo*"
+# #$ -l hostname=!scc-c*&!scc-t*
 #$ -l h_rt=48:00:00
 #$ -N TEST
 #cd /dummy/path
